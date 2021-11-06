fd291550c41b406c668866653518bcdefc92a769
LucasLBF <lucas.fernandes1454@gmail.com>
2021-11-06 13:53:38 -0300

    consertar erro do test_servicos_are_created

--- tests/test_novo_atendimento.py
+++ tests/test_novo_atendimento.py
@@ -1,35 +1,31 @@
-from app.models import Pet, Atendimento
-from app.controllers import ClienteController, PetController, AtendimentoController, ServicoController
+from app.controllers import AtendimentoController, ClienteController, ServicoController
 from tests.helpers import cadastrar_cliente_pet
 
-    
-# funcionario acessa a pagina de novo atendimento
-def test_request_returns_200(client):
-    assert client.get("/novo-atendimento").status_code == 200
-
-def test_servicos_are_created():
+def test_servicos_are_created(app):
     s_control = ServicoController()
     servicos = []
     servicos.append(s_control.get_servico_by_descricao("Banho Shihtzu"))
     servicos.append(s_control.get_servico_by_descricao("Hidratacao"))
     assert servicos[0].valor == 30.00 and servicos[1].valor == 10.00
 
+# funcionario acessa a pagina de novo atendimento
+def test_request_returns_200(client):
+    assert client.get("/novo-atendimento").status_code == 200
+
 # funcionario registra o atendimento de banho e hidratacao para um pet
 def test_registro_atendimento():
-    atendimento_controller = AtendimentoController()
-    servico_controller = ServicoController()
+    a_controller = AtendimentoController()
+    s_controller = ServicoController()
     cliente, pet = cadastrar_cliente_pet()
 
     # funcionario cria um novo atendimento para o cliente, que solicita
     # serviços de banho e hidratação para o seu pet 
-    atendimento = Atendimento(
-        status="Pendente",
-    )
+    atendimento = a_controller.create_atendimento("pendente")
     servicos = []
-    servicos.append(servico_controller.get_servico_by_descricao("Banho Shihtzu"))
-    servicos.append(servico_controller.get_servico_by_descricao("Hidratacao"))
-    atendimento_controller.add_atendimento(atendimento, pet, servicos)
-    novo_atendimento = Atendimento.query.filter_by(pet_id=pet.id).first()
+    servicos.append(s_controller.get_servico_by_descricao("Banho Shihtzu"))
+    servicos.append(s_controller.get_servico_by_descricao("Hidratacao"))
+    a_controller.add_atendimento(atendimento, pet, servicos)
+    novo_atendimento = a_controller.get_last_atendimento_by_id(pet.id)
     lista_servicos = []
     for atendimento in novo_atendimento.servicos.all():
         lista_servicos.append(atendimento.descricao)
