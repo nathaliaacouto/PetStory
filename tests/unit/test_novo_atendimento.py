2048ea1f24d92d0804e42d82b7e0ccbb4c137a7b
LucasLBF <lucas.fernandes1454@gmail.com>
2021-11-10 18:11:47 -0300

    reorganizar testes e adicionar funcionario em um novo atendimento

--- tests/unit/test_novo_atendimento.py
+++ tests/unit/test_novo_atendimento.py
@@ -1,4 +1,4 @@
-from app.controllers import AtendimentoController, ClienteController, ServicoController
+from app.controllers import AtendimentoController, ClienteController, ServicoController, FuncionarioController
 from tests.helpers import cadastrar_cliente_pet
 
 def test_servicos_are_created(app):
@@ -16,6 +16,14 @@ def test_request_returns_200(client):
 def test_registro_atendimento():
     a_controller = AtendimentoController()
     s_controller = ServicoController()
+    f_controller = FuncionarioController()
+    carlos = f_controller.create_funcionario({
+        "nome": "Carlos",
+        "email": "carlos@carlos.com",
+        "senha_hash": "1234",
+        "codigo": 56147,
+        "cargo": "OPERACOES"
+    })
     cliente, pet = cadastrar_cliente_pet()
 
     # funcionario cria um novo atendimento para o cliente, que solicita
@@ -24,9 +32,10 @@ def test_registro_atendimento():
     servicos = []
     servicos.append(s_controller.get_servico_by_descricao("Banho Shihtzu"))
     servicos.append(s_controller.get_servico_by_descricao("Hidratacao"))
-    a_controller.add_atendimento(atendimento, pet, servicos)
+    a_controller.add_atendimento(atendimento, pet, servicos, carlos)
     novo_atendimento = a_controller.get_last_atendimento_by_id(pet.id)
     lista_servicos = []
-    for atendimento in novo_atendimento.servicos.all():
-        lista_servicos.append(atendimento.descricao)
-    assert lista_servicos[0] == "Banho Shihtzu" and lista_servicos[1] == "Hidratacao"
+    for servico in novo_atendimento.servicos.all():
+        lista_servicos.append(servico.descricao +' '+ str(servico.valor))
+    assert lista_servicos[0] == "Banho Shihtzu 30.0" and lista_servicos[1] == "Hidratacao 10.0"
+    assert novo_atendimento.codigo_func == carlos.codigo
